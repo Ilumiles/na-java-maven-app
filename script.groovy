6ff8645cc83673ad4eb5e7b3ecba7cf4ea9cddb6
Ilumiles <61376503+Ilumiles@users.noreply.github.com>
2022-04-17 00:00:15 -0500

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -7,7 +7,7 @@ def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'nexus-docker-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t 104.248.112.133:8083/my-app:a-1.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin 104.248.112.133:8083"
+        sh "echo $PASS | docker login -u $USER --password-stdin  104.248.112.133:8083 "
         sh 'docker push 104.248.112.133:8083/my-app:a-1.0'
     }
 } 
