a8caf3d57189c6d77b3c7f1f0eb8c1dc6b94048d
Ilumiles <61376503+Ilumiles@users.noreply.github.com>
2022-04-17 00:11:17 -0500

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,11 +5,10 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'nexus-docker-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t 104.248.112.133:8083/my-app:a-1.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin  104.248.112.133:8083 "
-        sh 'docker push 104.248.112.133:8083/my-app:a-1.0'
-    }
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t ilumiles/my-repo-app:a-1.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin "
+        sh 'docker push ilumiles/my-repo-app:a-1.0'
 } 
 
 def deployApp() {
